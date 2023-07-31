4b47fc1bcad6c045fef6c0a5623c44c9e51a48c8
yurim0628 <139152515+yurim0628@users.noreply.github.com>
2023-07-31 21:07:49 +0900

    백준 1158번: 요세푸스 문제 문제풀이

--- B1158_Queue.py
+++ B1158_Queue.py
@@ -1,10 +1,10 @@
 N, K = map(int, input().split())
-circle = [ i for i in range(1, N+1)]
+circle_q = [ i for i in range(1, N+1)]
 result = []
 
 index = 0
-while circle:
-  index = (index + (K-1)) % len(circle)
-  result.append(circle.pop(index))
+while circle_q:
+  index = (index + (K-1)) % len(circle_q)
+  result.append(circle_q.pop(index))
 
 print("<" + ", ".join(map(str, result)) + ">") 
