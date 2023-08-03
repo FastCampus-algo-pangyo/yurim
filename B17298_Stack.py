092607640130f28fbb05c8226a2ecd6ca3f4b715
yurim0628 <139152515+yurim0628@users.noreply.github.com>
2023-08-03 19:31:36 +0900

    백준 17298번: 오큰수 문제풀이

---
+++ B17298_Stack.py
@@ -0,0 +1,12 @@
+N = int(input())
+A = list(map(int, input().split()))
+
+answer = [-1] * N
+stack = [0]
+
+for i in range(1, N):
+    while stack and A[stack[-1]] < A[i]:
+        answer[stack.pop()] = A[i]
+    stack.append(i)
+
+print(*answer)
