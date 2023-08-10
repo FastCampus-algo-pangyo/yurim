34ac0c80d8e58b508324782d39fc7a7fa1dc4f73
yurim0628 <139152515+yurim0628@users.noreply.github.com>
2023-08-10 12:03:01 +0900

    백준 1105번: 팔 문제풀이

--- B1105_Greedy.py
+++ B1105_Greedy.py
@@ -2,14 +2,13 @@ l, r = input().split()
 count = 0
 
 if ("8" not in l and "8" not in r) or (len(l) != len(r)):
-  print(0)
+    print(0)
 else:
-  for l_num, r_num in zip(l, r):
-    if l_num == r_num:
-      if l_num == "8":
-        count +=1
-    else:
-      break
-print(count)
-
+    for l_num, r_num in zip(l, r):
+        if l_num == r_num:
+            if l_num == "8":
+                count +=1
+        else:
+            break
+    print(count)
 
