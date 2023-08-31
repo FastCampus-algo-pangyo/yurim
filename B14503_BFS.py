5994798546f29ae77af04cf740f666ef9e254d46
yurim0628 <139152515+yurim0628@users.noreply.github.com>
2023-08-31 12:38:42 +0900

    백준 14503번: 로봇 청소기 문제풀이

---
+++ B14503_BFS.py
@@ -0,0 +1,39 @@
+from collections import deque
+
+n, m = map(int, input().split())
+sx, sy, d = map(int, input().split())
+room = [list(map(int, input().split())) for _ in range(n)]
+
+count = 1
+queue = deque([(sx, sy)])  
+visited = [[0] * m for _ in range(n)]
+visited[sx][sy] = 1
+
+dx = [-1, 0, 1, 0]
+dy = [0, 1, 0, -1]
+
+def bfs():
+    global d, count
+
+    while queue:
+        cx, cy = queue.popleft()
+        
+        for _ in range(4):
+            d = (d + 3) % 4
+            nx, ny = cx + dx[d], cy + dy[d]
+            
+            if 0 <= nx < n and 0 <= ny < m and visited[nx][ny] == 0 and room[nx][ny] == 0:
+                queue.append((nx, ny))
+                visited[nx][ny] = 1
+                count += 1
+                break
+        else:
+            nx, ny = cx - dx[d], cy - dy[d]
+            if 0 <= nx < n and 0 <= ny < m and room[nx][ny] == 0:
+                queue.append((nx, ny))
+            else:
+                return count
+    
+    return count
+
+print(bfs())
