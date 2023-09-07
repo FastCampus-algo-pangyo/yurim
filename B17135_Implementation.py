8347e2ccc4b51d067b575261e69b9e773df18090
yurim0628 <139152515+yurim0628@users.noreply.github.com>
2023-09-07 18:03:34 +0900

    백준 17135번: 캐슬 디펜스 문제풀이

--- B17135_Implementation.py
+++ B17135_Implementation.py
@@ -9,19 +9,19 @@ def solution(archer_positions):
     archer_x, archer_y = archer_position[0], archer_position[1]
     attack = []
     
-    for x in range(archer_x): # 현재 궁수가 위치한 줄까지만
+    for x in range(archer_x): 
       for y in range(m):
-        if map_copy[x][y] == 1: # 적이 있을 경우
-          distance = abs(x - archer_x) + abs(y - archer_y) # 궁수와 적의 위치를 계산
-          if (distance <= d): # 반경 내에 존재할 경우
-            attack.append((x, y, distance)) # 공격 가능 리스트에 추가
+        if map_copy[x][y] == 1: 
+          distance = abs(x - archer_x) + abs(y - archer_y)
+          if (distance <= d): 
+            attack.append((x, y, distance)) 
 
     if attack:
-      attack_list.append(sorted(attack, key=lambda x: (x[2],x[1]))[0]) # 가장 가까운 적이고, 가장 왼쪽에 있는 적을 공격
+      attack_list.append(sorted(attack, key=lambda x: (x[2],x[1]))[0]) 
 
-  for x, y, distance in attack_list: # 각 궁수마다 공격할 적의 위치를 파악
-    if map_copy[x][y] == 1: # 모든 궁수는 동시에 공격한다. > 같은 적이 여러 궁수에게 공격당할 수 있다.
-      map_copy[x][y] = 0 # 공격 받은 곳은 적을 제거시켜서 카운트 값이 여러번 증가하지 않도록 만든다.
+  for x, y, distance in attack_list:
+    if map_copy[x][y] == 1: 
+      map_copy[x][y] = 0 
       count += 1 
 
   return count
@@ -36,7 +36,7 @@ for archer_y in combinations(archer_y, 3):
     count = 0
     map_copy = copy.deepcopy(map)  
   
-    for archer_x in range(n, 0, -1): # 적을 밑으로 이동시키는 것이 아니라, 궁수를 위로 이동시키는 게 훨씬 효율적이다. (x 값만 변화).
+    for archer_x in range(n, 0, -1):
       archer_positions = [(archer_x, archer_y[0]), (archer_x, archer_y[1]), (archer_x, archer_y[2])]
       count += solution(archer_positions)
     
